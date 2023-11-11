8e6b3b77c072d39988a0549f4931d6378c9f0bf9
unknown <nguyencongtanphat1232002@gmail.com>
2023-11-11 10:41:14 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,129 +1,139 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import ErrorPage from "./error-page";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import ErrorPage from './error-page';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 //main route here
-import DashBoard from "./modules/dashboard/dashboard";
-import PendingPost, { loader as pendingPostLoader} from "./modules/post/screens/PendingPost";
-import ApprovedPost, {loader as approvedPostLoader} from "./modules/post/screens/ApprovedPost";
-import RejectedPost, {loader as rejectedPostLoader} from "./modules/post/screens/RejectedPost";
-import {action as postAction} from "./modules/post/action"
-import Root from "./Root";
-import Package from "./modules/package/screens/Package"
-import Voucher from "./modules/voucher/screens/Voucher"
-import PendingReporting from "./modules/reporting/screens/PendingReporting"
-import ApprovedReporting from "./modules/reporting/screens/ApprovedReporting"
-import User from "./modules/user/screens/User"
-import PendingUser from "./modules/user/screens/PendingUser"
-import VertificatedUser from "./modules/user/screens/VertificatedUser"
-import Blog from "./modules/blog/screens/Blog"
+import DashBoard from './modules/dashboard/dashboard';
+import PendingPost, {
+  loader as pendingPostLoader,
+} from './modules/post/screens/PendingPost';
+import ApprovedPost, {
+  loader as approvedPostLoader,
+} from './modules/post/screens/ApprovedPost';
+import RejectedPost, {
+  loader as rejectedPostLoader,
+} from './modules/post/screens/RejectedPost';
+import { action as postAction } from './modules/post/action';
+import Root from './Root';
+import Package from './modules/package/screens/Package';
+import Voucher from './modules/voucher/screens/Voucher';
+import PendingReporting from './modules/reporting/screens/PendingReporting';
+import ApprovedReporting from './modules/reporting/screens/ApprovedReporting';
+import User from './modules/user/screens/User';
+import PendingUser from './modules/user/screens/PendingUser';
+import VertificatedUser from './modules/user/screens/VertificatedUser';
+import Blog from './modules/blog/screens/Blog';
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <Root/>,
+    path: '/',
+    element: <Root />,
     errorElement: <ErrorPage />,
-    children:[
-      {  
-        children:[
+    children: [
+      {
+        children: [
           {
-            index: true, 
-            element: <DashBoard /> 
+            index: true,
+            element: <DashBoard />,
           },
           {
-            path: "pending_post",
+            path: 'pending_post',
             element: <PendingPost />,
             loader: pendingPostLoader,
             action: postAction,
           },
           {
-            path:"approved_post",
+            path: 'approved_post',
             element: <ApprovedPost />,
             loader: approvedPostLoader,
             action: postAction,
           },
           {
-            path:"rejected_post",
+            path: 'rejected_post',
             element: <RejectedPost />,
             loader: rejectedPostLoader,
             action: postAction,
           },
           {
-            path: "package",
+            path: 'package',
             element: <Package />,
           },
           {
-            path: "voucher",
+            path: 'voucher',
             element: <Voucher />,
           },
           {
-            path: "pending_reporting",
-            element: <PendingReporting/>,
+            path: 'pending_reporting',
+            element: <PendingReporting />,
           },
           {
-            path: "approved_reporting",
-            element: <ApprovedReporting/>,
+            path: 'approved_reporting',
+            element: <ApprovedReporting />,
           },
           {
-            path: "user",
-            element: <User/>,
+            path: 'user',
+            element: <User />,
           },
           {
-            path: "verificated_user",
-            element: <VertificatedUser/>,
+            path: 'verificated_user',
+            element: <VertificatedUser />,
           },
           {
-            path: "pending_user",
-            element: <PendingUser/>,
+            path: 'pending_user',
+            element: <PendingUser />,
           },
           {
-            path: "blog",
-            element: <Blog/>,
+            path: 'blog',
+            element: <Blog />,
           },
           // {
           //   path: "approved_post",
           //   element: <ApprovedPost />,
           // },
           {
-            path: "package",
+            path: 'package',
             element: <Package />,
           },
           {
-            path: "voucher",
+            path: 'voucher',
             element: <Voucher />,
           },
           {
-            path: "pending_reporting",
-            element: <PendingReporting/>,
+            path: 'pending_reporting',
+            element: <PendingReporting />,
           },
           {
-            path: "approved_reporting",
-            element: <ApprovedReporting/>,
+            path: 'approved_reporting',
+            element: <ApprovedReporting />,
           },
           {
-            path: "user",
-            element: <User/>,
+            path: 'user',
+            element: <User />,
           },
           {
-            path: "verificated_user",
-            element: <VertificatedUser/>,
+            path: 'verificated_user',
+            element: <VertificatedUser />,
           },
           {
-            path: "pending_user",
-            element: <PendingUser/>,
+            path: 'pending_user',
+            element: <PendingUser />,
           },
           {
-            path: "blog",
-            element: <Blog/>,
+            path: 'blog',
+            element: <Blog />,
           },
-        ]
-      }
-    ]
-  }
-])
+          {
+            path: 'blog/add',
+            element: <h1>ADD PAGE</h1>,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
