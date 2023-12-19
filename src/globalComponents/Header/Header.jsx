f23ec758b6c15a5a713056294202565e04a8474a
unknown <nguyencongtanphat1232002@gmail.com>
2023-12-19 22:40:15 +0700

    updating

--- src/globalComponents/Header/Header.jsx
+++ src/globalComponents/Header/Header.jsx
@@ -1,36 +1,44 @@
-import React from 'react'
-import { BellOutlined, MailOutlined, CaretDownOutlined } from "@ant-design/icons";
-import { UserOutlined } from "@ant-design/icons";
+import React from 'react';
+import {
+  BellOutlined,
+  MailOutlined,
+  CaretDownOutlined,
+} from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
 import { Row, Col, Select, Button } from 'antd';
 
-import {
-  Badge,
-  Image,
-  Avatar,
-  Space,
-  Divider
-} from "antd";
+import { Badge, Image, Avatar, Space, Divider } from 'antd';
 import style from './Header.module.css';
 import logo from '../../../assets/Logo.png';
 
 function Header() {
-  
   return (
     <div className={style.AppHeader}>
-        <img src={logo} alt="" style={{height:"40px", paddingTop:"8px", paddingBottom:"8px", paddingLeft:"32px"}}/>
+      <img
+        src={logo}
+        alt=""
+        style={{
+          height: '40px',
+          paddingTop: '8px',
+          paddingBottom: '8px',
+          paddingLeft: '32px',
+        }}
+      />
       <Space>
         <Badge count={3} dot>
-          <BellOutlined
-            style={{ fontSize: 24 }}
-          />
+          <BellOutlined style={{ fontSize: 24 }} />
         </Badge>
         <Divider type="vertical" style={{ width: 3, height: 30 }} />
-        <Avatar size={36} icon={<UserOutlined />} style={{backgroundColor:"#026D4D"}} />
-         <Row>Jun Nguyễn</Row>
+        <Avatar
+          size={36}
+          icon={<UserOutlined />}
+          style={{ backgroundColor: '#026D4D' }}
+        />
+        <Row>Admin</Row>
         <CaretDownOutlined />
       </Space>
     </div>
   );
 }
 
-export default Header
+export default Header;
--- src/modules/package/screens/Package.jsx
+++ src/modules/package/screens/Package.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import {Tabs, Card, Row, Col, Typography, Button, Select, Table, Space } from 'antd';
+import {
+  Tabs,
+  Card,
+  Row,
+  Col,
+  Typography,
+  Button,
+  Select,
+  Table,
+  Space,
+} from 'antd';
 import Breadcrumbs from '../../../globalComponents/BreadCrumb/BreadCrumb';
 import Search from 'antd/es/input/Search';
 import { useState, useRef, useEffect } from 'react';
@@ -7,102 +17,99 @@ import { useNavigate, useFetcher, useLoaderData } from 'react-router-dom';
 import PostTable from '../components/Table';
 import { Tag } from 'antd';
 import ApiService from '../../../service/ApiService';
+import moment from 'moment';
 
 export async function loader() {
-  
-  const voucher = await ApiService.get("membership-packages");
-  console.log("length",voucher.length);
+  const voucher = await ApiService.get('membership-packages');
+  console.log('length', voucher.length);
   if (!voucher) {
-    throw new Response("", {
+    throw new Response('', {
       status: 404,
-      statusText: "Not Found",
+      statusText: 'Not Found',
     });
   }
   //const postLease = posts.filter(post => post.is_lease === true);
   //const postNoLease = posts.filter(post => post.is_lease === false);
   // console.log("lease", postLease)
   // console.log("no lease", postNoLease)
-  console.log("voucher: ",voucher)
-  return { voucher }; 
+  console.log('voucher: ', voucher);
+  return { voucher };
 }
 
-
 function PendingPost(props) {
-
-const columns = [
-  {
-    title: "Tên",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Mô tả",
-    dataIndex: "description",
-    key: "description",
-  },
-  {
-    title: "Giá/tháng",
-    dataIndex: "price_per_month",
-    sorter: (a, b) => a.price_per_month - b.price_per_month,
-    key: "price_per_month",
-  },
-  {
-    title: "Số lượng bài đăng/tháng",
-    dataIndex: "monthly_post_limit",
-    sorter: (a, b) => a.monthly_post_limit - b.monthly_post_limit,
-    key: "monthly_post_limit",
-  },
-  {
-    title: "Ngày tạo",
-    dataIndex: "created_at",
-    key: "created_at",
-  },
-  {
-    title: "Điểm ưu tiên hiện bài",
-    dataIndex: "display_priority_point",
-    key: "display_priority_point",
-  },
-  {
-    title: "Điểm ưu tiên duyệt bài",
-    dataIndex: "post_approval_priority_point",
-    key: "post_approval_priority_point",
-  },
-  {
-    title: "Trạng thái",
-    dataIndex: "is_active",
-    key: "is_active",
-    render: (is_active) => (
+  const columns = [
+    {
+      title: 'Tên',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Mô tả',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Giá/tháng',
+      dataIndex: 'price_per_month',
+      sorter: (a, b) => a.price_per_month - b.price_per_month,
+      key: 'price_per_month',
+    },
+    {
+      title: 'Số lượng bài đăng/tháng',
+      dataIndex: 'monthly_post_limit',
+      sorter: (a, b) => a.monthly_post_limit - b.monthly_post_limit,
+      key: 'monthly_post_limit',
+    },
+    {
+      title: 'Ngày tạo',
+      dataIndex: 'created_at',
+      key: 'created_at',
+      render: (date) => moment(date).format('hh:mm DD/MM/YYYY'),
+    },
+    {
+      title: 'Điểm ưu tiên hiện bài',
+      dataIndex: 'display_priority_point',
+      key: 'display_priority_point',
+    },
+    {
+      title: 'Điểm ưu tiên duyệt bài',
+      dataIndex: 'post_approval_priority_point',
+      key: 'post_approval_priority_point',
+    },
+    {
+      title: 'Trạng thái',
+      dataIndex: 'is_active',
+      key: 'is_active',
+      render: (is_active) => (
         <span>
           {
-            
-              <Tag color={is_active ? "green" : "red"} key={is_active}>
-                {is_active ? "Đang kích hoạt" : "Vô hiệu"}
-              </Tag>
-            
+            <Tag color={is_active ? 'green' : 'red'} key={is_active}>
+              {is_active ? 'Đang kích hoạt' : 'Vô hiệu'}
+            </Tag>
           }
         </span>
       ),
     },
-];
+  ];
   const fetcher = useFetcher();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { Title } = Typography;
-  const { voucher } = useLoaderData()
-  
+  const { voucher } = useLoaderData();
+
   return (
     <div>
       <Card>
-      <Breadcrumbs/>
-        <Row style={{marginBottom:"16px"}}>
+        <Breadcrumbs />
+        <Row style={{ marginBottom: '16px' }}>
           <Col>
             <Title level={3} style={{ margin: 0, padding: 0 }}>
-            Gói dịch vụ
+              Gói dịch vụ
             </Title>
           </Col>
         </Row>
-        <Row style={{marginBottom:"12px"}}>
+        <Row style={{ marginBottom: '12px' }}>
           <Col>
-              <Search
+            <Search
               placeholder="Nhập thông tin cần tìm..."
               style={{
                 width: 500,
@@ -110,12 +117,11 @@ const columns = [
               onSearch={() => {}}
               enterButton
             />
-            </Col>
+          </Col>
         </Row>
-    
+
         <PostTable columns={columns} data={voucher} />
       </Card>
-      
     </div>
   );
 }
